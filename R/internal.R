ef1ae94c815808bc7a3304ece30314079dc2e27b
SandroGroth <sandro_groth@outlook.com>
2019-04-17 21:49:59 +0200

    Removed parameters from GET method.

--- R/internal.R
+++ R/internal.R
@@ -8,11 +8,10 @@
 #' @keywords internal
 #' @noRd
 
-esriOpenData.call_get <- function(url, parameters=list()) {
+esriOpenData.call_get <- function(url) {
   if (!is.character(url)) {msg("url has to be of type character", "ERROR")}
-  if (!is.list(parameters)) {msg("parameters has to be of type list.", "ERROR")}
 
-  response <- httr::GET(url, parameters)
+  response <- httr::GET(url)
   msg(paste0("Response status: ", response$status_code))
 
   if(response$status_code != 200) {msg(paste0("Failed to process request. Status code: ", response$status_code))}
