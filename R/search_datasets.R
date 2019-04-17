9f7c5a2b14c72df5e23df455121df1781bf25171
SandroGroth <sandro_groth@outlook.com>
2019-04-17 21:51:01 +0200

    Implemented correct encoding of request parameters in search_dirs using urltools.

--- R/search_datasets.R
+++ R/search_datasets.R
@@ -2,11 +2,14 @@
 #'
 #' @export
 
-search_datasets <- function(category=c(), name="", source="", sector="", region="", aoi=FALSE) {
+search_datasets <- function(category="", name="", source="", sector="", region="", aoi=FALSE) {
   url <- paste0(getOption("esriOpenData.api_base"), getOption("esriOpenData.api_dataset_endpoint"))
-  parameters = list()
-  if(length(category) != 0) {rlist::list.append(parameters, q = category)}
-  response <- esriOpenData.call_get(url, parameters)
+
+  url <- urltools::param_set(url, "q", category)
+  url <- urltools::param_set(url, "page[size]", 1000)
+  msg(url, "INFO")
+
+  response <- esriOpenData.call_get(url)
   response_json <- jsonlite::fromJSON(response)
   return(response_json)
 }
