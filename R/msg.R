dc7bce898d933e3eab25da7157591b5cdc2e2263
SandroGroth <sandro_groth@outlook.com>
2019-04-19 17:06:53 +0200

    Added quit statements in msg. Implemented further parameter setting.

--- R/msg.R
+++ R/msg.R
@@ -10,6 +10,7 @@
 msg <- function(message, level="INFO", tag="") {
   if (level == "ERROR") {
     stop(paste0(tag, " ", message), call. = FALSE, immediate. = TRUE)
+    quit("no")
   }
   if (level == "WARNING") {
     warning(paste0(tag, " ", message))
--- R/search_datasets.R
+++ R/search_datasets.R
@@ -2,12 +2,18 @@
 #'
 #' @export
 
-search_datasets <- function(category="", name="", source="", sector="", region="", aoi=FALSE) {
+search_datasets <- function(query="", source="", sector="", region="", aoi=FALSE) {
+  if(!is.character(query)) {msg("Query has to be of type character.", "ERROR")}
+
   url <- paste0(getOption("esriOpenData.api_base"), getOption("esriOpenData.api_dataset_endpoint"))
 
-  url <- urltools::param_set(url, "q", category)
+  if(query != "") {url <- urltools::param_set(url, "q", query)}
+  if(source != "") {url <- urltools::param_set(url, "source", source)}
+  if(sector != "") {url <- urltools::param_set(url, "sector", sector)}
+  if(region != "") {url <- urltools::param_set(url, "region", region)}
   url <- urltools::param_set(url, "page[size]", 1000)
-  msg(url, "INFO")
+  url <- urltools::param_set(url, "collection", "datasets")
+  msg(paste0("Requesting URL: ", url, "INFO"))
 
   response <- esriOpenData.call_get(url)
   response_json <- jsonlite::fromJSON(response)
